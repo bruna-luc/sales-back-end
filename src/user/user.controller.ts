6fa6980c37875fd048a2537277b7985b00a8fe12
Bruna Oliveira <79415713+bruna-luc@users.noreply.github.com>
2023-06-19 10:20:34 -0300

    create user dtos and the folder interfaces

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
+  // @Get()
+  // async getAllUser() {
+  //   return JSON.stringify({ test: 'abc' });
+  // }
 
-
+  @Post()
+  async createUser(@Body() createUser: createUserDto) {
+    return {
+      ...createUser,
+      password: 'undefined',
+    };
   }
 }
